e69db2f3989fc0c552ca2cb2de045c4e33270e64
Andrey Agafonov <aagafonov@inbox.ru>
2020-12-19 23:38:37 +0300

    install mongo enable debug fix fetch repo

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -2,11 +2,11 @@
 echo -e "\e[6;34m Run isntall MongoDb"
 tput sgr0
 echo "Get Repo key"
-bash -x wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-bash -x echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-4.2.list
-
-sudo apt-get update -qq
-sudo apt-get install -qq -y mongodb-org
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+apt-get install apt-transport-https ca-certificates
+sudo apt-get update
+sudo apt-get install -y mongodb-org
 if [[ $? == 0 ]]
         then
           printf '\e[7;32m%-6s\e[m' "Install Succses"
